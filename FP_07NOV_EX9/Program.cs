00068e29ec82fd84ec96c8a2bc4ffe3d1056fcaa
renata <renata@DESKTOP-7K85QAI>
2022-11-20 11:07:56 +0200

    Add project files.

---
+++ FP_07NOV_EX9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace oglinditul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, p, q, min, max;
+
+            Console.WriteLine("p =");
+            p = int.Parse(Console.ReadLine());
+            Console.WriteLine("q =");
+            q = int.Parse(Console.ReadLine());
+            
+            if (p < q)
+            {
+                min = p;
+                max = q;
+            }
+            else
+            {
+                min = q;
+                max = p;
+            }
+            for (i = 1; i <= min; i++)
+            {
+                if (min % i == 0)
+                    if (max % 1 == 0)
+                        Console.WriteLine($"{i}");
+            }
+        }
+    }
+}
